da169860a9ec71d02433f5c1117abdc1df0fa8c6
tsi <timo42@proton.me>
2025-04-17 04:31:01 +0200

    Add another function

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -2,5 +2,9 @@
 #include <iostream>
 
 static inline void print(char c) { std::cout << c << std::endl; }
+static inline void print_fancy(char c) { std::cout << "~~~ " << c << " ~~~" << std::endl; }
 
-int main() { iter("Hello, World!", 13, print); }
+int main() {
+    iter("Hello, World!", 13, print);
+    iter("Hello, World!", 13, print_fancy);
+}
